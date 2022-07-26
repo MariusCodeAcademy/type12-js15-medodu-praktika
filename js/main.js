b8384311a6629131753e28aee6656e372036eea6
Marius <marius@example.com>
2022-07-26 14:03:44 +0300

    didziausias

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const numbers = [1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45]
 
 // 1. rasti didziausia skaiciu su reduce
 
+const didziausias = numbers.reduce((maxSk, sk) => Math.max(maxSk, sk), numbers[0]);
+console.log('didziausias ===', didziausias);
 // 2. Sukurkite ir atvaizduokite masyvą su dvigubomis reikšmėmis;
 
 // 3. Sukurkite ir atvaizduokite masyvą su reikšmėmis padaugintomis iš jų vietos masyve indekso

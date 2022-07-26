3c11c1f5c60057e9481486ebf3c71165cfb583b1
Marius <marius@example.com>
2022-07-26 13:56:51 +0300

    stud

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ const numbers = [1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45]
 // 5. grazinti nauja masyva su Suapvalintom reikšmem iki sveikų skaičių
 
 // 6. Atrinkti kas antrą elementą į naują masyvą(ne su for i)
+
+// 7. Grazinti nauja masyva kuriame yra objektai
+// [
+//   {index: 0, reiksme: 1}
+//   {index: 1, reiksme: 2}
+//    ...
+// ]
---
+++ js/students.js
@@ -0,0 +1,58 @@
+const students = [
+  {
+    name: 'James',
+    gender: 'male',
+    age: 25,
+    town: 'Vilnius',
+    hasCar: true,
+  },
+  {
+    name: 'Jill',
+    gender: 'female',
+    age: 28,
+    town: 'Kaunas',
+    hasCar: true,
+  },
+  {
+    name: 'Mike',
+    gender: 'male',
+    age: 18,
+    town: 'Kaunas',
+    hasCar: false,
+  },
+  {
+    name: 'Jane',
+    gender: 'female',
+    age: 22,
+    town: 'Klaipeda',
+    hasCar: false,
+  },
+  {
+    name: 'Jannet',
+    gender: 'female',
+    age: 29,
+    town: 'Klaipeda',
+    hasCar: true,
+  },
+  {
+    name: 'Cory',
+    gender: 'male',
+    age: 30,
+    town: 'Siauliai',
+    hasCar: true,
+  },
+  {
+    name: 'Minks',
+    gender: 'male',
+    age: 19,
+    town: 'Siauliai',
+    hasCar: false,
+  },
+];
+
+// st1. sugeneruoti studentu nerikiuota sarasa htmle kuriame butu studentu vardai
+// st1.1 parasyti funkcija kuri atlieka [st1]
+
+// st2. Parasyti funkcija, kuriai paduodam miesta kaip argumenta ir ji atrenka/grazina studentus is to miesto
+
+// st3. Parasyti funkcija, kuriai paduodam amziu kaip argumenta ir ji atrenka studentus kuriu amzius didesnis uz argumenta.
